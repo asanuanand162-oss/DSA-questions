78f54ee65c1f37acb457b366f761c30d99fd6771
aanchal singh <asanuanand162@gmail.com>
2026-06-25 00:30:28 +0530

    trying to survive

---
+++ dsa1_question.py
@@ -0,0 +1,6 @@
+print("Complete the function printNumber which takes an integer input from the user and prints it on the screen.")
+class Solution:
+    def printNumber(self):
+        a=int(input())
+        print(a)
+
